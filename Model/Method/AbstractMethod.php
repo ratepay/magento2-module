c5821f4e64d5e1129f4988dcfd4b1e5591234489
Robert Müller <robert.mueller@fatchip.de>
2020-09-23 13:30:24 +0200

    M2-63 - Fixed config scope for backend orders

--- Model/Method/AbstractMethod.php
+++ Model/Method/AbstractMethod.php
@@ -330,25 +330,24 @@ abstract class AbstractMethod extends \Magento\Payment\Model\Method\AbstractMeth
             return false;
         }
 
-        if (!$this->rpDataHelper->getRpConfigData($this->_code, 'active')) {
+        if (!$this->rpDataHelper->getRpConfigDataForQuote($this->_code, 'active', $quote)) {
             return false;
         }
 
         $totalAmount = $quote->getGrandTotal();
-        $minAmount = $this->rpDataHelper->getRpConfigData($this->_code, 'min_order_total');
-        $maxAmount = $this->rpDataHelper->getRpConfigData($this->_code, 'max_order_total');
+        $minAmount = $this->rpDataHelper->getRpConfigDataForQuote($this->_code, 'min_order_total', $quote);
+        $maxAmount = $this->rpDataHelper->getRpConfigDataForQuote($this->_code, 'max_order_total', $quote);
 
         if ($totalAmount < $minAmount || $totalAmount > $maxAmount) {
             return false;
         }
 
-
         $address = $quote->getBillingAddress();
-        if (!$this->canUseForCountryDelivery($address->getCountryId())) {
+        if (!$this->canUseForCountryDelivery($address->getCountryId(), $quote)) {
             return false;
         }
 
-        $aValidCurrencies = explode(',', $this->rpDataHelper->getRpConfigData($this->_code, 'currency'));
+        $aValidCurrencies = explode(',', $this->rpDataHelper->getRpConfigDataForQuote($this->_code, 'currency', $quote));
         if (in_array($quote->getQuoteCurrencyCode(), $aValidCurrencies) === false) {
             return false;
         }
@@ -359,12 +358,13 @@ abstract class AbstractMethod extends \Magento\Payment\Model\Method\AbstractMeth
     /**
      * To check billing country is allowed for the payment method
      *
-     * @param $country
+     * @param                                       $country
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
      * @return bool
      */
-    public function canUseForCountryDelivery($country)
+    public function canUseForCountryDelivery($country, \Magento\Quote\Api\Data\CartInterface $quote = null)
     {
-        $availableCountries = explode(',', $this->rpDataHelper->getRpConfigData($this->_code, 'specificcountry_delivery'));
+        $availableCountries = explode(',', $this->rpDataHelper->getRpConfigDataForQuote($this->_code, 'specificcountry_delivery', $quote));
         if(!in_array($country, $availableCountries)){
             return false;
         }
