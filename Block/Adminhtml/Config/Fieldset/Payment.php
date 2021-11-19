1457b47862e7a32dc3c95cc9516d54c20f06a3f8
Robert Müller <robert.mueller@fatchip.de>
2021-11-19 13:15:23 +0100

    Added module version display to backend

--- Block/Adminhtml/Config/Fieldset/Payment.php
+++ Block/Adminhtml/Config/Fieldset/Payment.php
@@ -8,6 +8,30 @@ namespace RatePAY\Payment\Block\Adminhtml\Config\Fieldset;
 class Payment extends \Magento\Config\Block\System\Config\Form\Fieldset
 {
     /**
+     * @var \Magento\Framework\Module\ModuleListInterface
+     */
+    protected $moduleList;
+
+    /**
+     * @param \Magento\Backend\Block\Context $context
+     * @param \Magento\Backend\Model\Auth\Session $authSession
+     * @param \Magento\Framework\View\Helper\Js $jsHelper
+     * @param \Magento\Framework\Module\ModuleListInterface $moduleList,
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Context $context,
+        \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Framework\View\Helper\Js $jsHelper,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        array $data = []
+    ) {
+        parent::__construct($context, $authSession, $jsHelper, $data);
+        $this->moduleList = $moduleList;
+
+    }
+
+    /**
      * Add custom css class
      *
      * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
@@ -29,15 +53,23 @@ class Payment extends \Magento\Config\Block\System\Config\Form\Fieldset
     {
         $htmlId = $element->getHtmlId();
 
-        $html = '<div class="config-heading" >';
-        $html .= '<div class="button-container"><button type="button" class="button action-configure" id="'.$htmlId.'-head" onclick="paypalToggleSolution.call(this, \''.$htmlId."', '".$this->getUrl('adminhtml/*/state').'\'); return false;">' .
-            '<span class="state-closed">' . __('Configure') . '</span>' .
-            '<span class="state-opened">' . __('Close') . '</span></button>';
+        $html  = '<div class="config-heading" >';
+        $html .= '    <div class="button-container">';
+        $html .= '        <button type="button" class="button action-configure" id="'.$htmlId.'-head" onclick="ratepayToggleSolution.call(this, \''.$htmlId."', '".$this->getUrl('adminhtml/*/state').'\'); return false;">';
+        $html .= '            <span class="state-closed">' . __('Configure') . '</span>';
+        $html .= '            <span class="state-opened">' . __('Close') . '</span>';
+        $html .= '        </button>';
+        $html .= '    </div>';
+        $html .= '    <div class="heading">';
+        $html .= '        <strong>' . $element->getLegend() . '</strong>';
+        $html .= '        <span class="heading-intro">';
+        $html .= '            <div class="ratepay-logo"></div>';
+        $html .= '            <div class="payment-text">' . $element->getComment() . '</div>';
+        $html .= '        </span>';
+        $html .= '        <div class="config-alt"></div>';
+        $html .= '    </div>';
+        $html .= '    <div id="ratepay_version_number">'.__('Module version').': '.$this->moduleList->getOne('RatePAY_Payment')['setup_version'].'</div>';
         $html .= '</div>';
-        $html .= '<div class="heading"><strong>' . $element->getLegend() . '</strong>';
-        $html .= '<span class="heading-intro"><div class="ratepay-logo"></div><div class="payment-text">' . $element->getComment() . '</div></span>';
-        $html .= '<div class="config-alt"></div>';
-        $html .= '</div></div>';
 
         return $html;
     }
@@ -76,9 +108,10 @@ class Payment extends \Magento\Config\Block\System\Config\Form\Fieldset
     protected function _getExtraJs($element)
     {
         $script = "require(['jquery', 'prototype'], function(jQuery){
-            window.paypalToggleSolution = function (id, url) {
+            window.ratepayToggleSolution = function (id, url) {
                 var doScroll = false;
                 Fieldset.toggleCollapse(id, url);
+                jQuery('#ratepay_version_number').toggle();
                 if ($(this).hasClassName(\"open\")) {
                     \$$(\".with-button button.button\").each(function(anotherButton) {
                         if (anotherButton != this && $(anotherButton).hasClassName(\"open\")) {
