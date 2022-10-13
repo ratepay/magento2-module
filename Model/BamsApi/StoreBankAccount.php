3f7f1bb2527f399f47098593a5827f54b4c64c2f
Robert Müller <robert.mueller@fatchip.de>
2022-10-13 17:22:02 +0200

    M2-92 - Added copyright headers to BAMS files

--- Model/BamsApi/StoreBankAccount.php
+++ Model/BamsApi/StoreBankAccount.php
@@ -1,8 +1,13 @@
 <?php
 
+/**
+ * Copyright (c) Ratepay GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace RatePAY\Payment\Model\BamsApi;
-
 
 class StoreBankAccount extends Base
 {
